960a4da696d816742c2e461210abb09bb7d7e526
Julien Choay <julien.choay@cirkwi.com>
2023-11-14 11:44:59 +0100

    [MEP] Augmentation des constantes

--- app/config/constantes.php
+++ app/config/constantes.php
@@ -63,7 +63,7 @@ $constantes = new Constantes(
         'DELAI_NOTIFICATIONS' => 60000, // en millisecondes
         'TIMEOUT_NOTIFICATIONS' => 5000, // temps maximum pour la requête en millisecondes
         'DEFAULT_ERREUR_DEV' => 'Ton application chie dans la colle',
-        'VERSION_CSS' =>  constant("ENV") == "prod" ? 576 : time(),
+        'VERSION_CSS' =>  constant("ENV") == "prod" ? 577 : time(),
         'VERSION_JS' =>  constant("ENV") == "prod" ? 576 : time(), // Permet de recharger le fichier javascript sans vider le cache
         'VERSION_JS_BACK' => constant("ENV") == "prod" ? 557 : time(), //Permet de forcer les js en cache du client
         'VERSION_JS_FRONT' => constant("ENV") == "prod" ? 557 : time(), //Permet de forcer les js en cache du client
