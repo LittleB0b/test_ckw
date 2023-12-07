ea18c41a7c9ac1099184869907d2d07f71eadf8f
Julien Choay <julien.choay@cirkwi.com>
2023-12-07 16:07:52 +0100

    Ajout scss

--- config.rb
+++ config.rb
@@ -1,14 +1,9 @@
 require "compass/import-once/activate"
-require "font-awesome-sass"
 
 environment = "production"
 
 css_dir = "web/css"
 
 sass_dir = "web/css/scss"
-
-fonts_dir = "web/css/fonts"
-
-images_dir = "web/images"
 
 relative_assets = true
